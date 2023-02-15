0a03d40f1cd0a2e93f070c706801ffebccb9f52f
EC2 Default User <mikehallvideo@gmail.com>
2023-02-15 03:09:13 +0000

    added login page

---
+++ app/AppLoginPage.jsx
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import styled from "styled-components";
+
+import {useGoogleLogin} from '@react-oauth/google';
+
+import logo from './logo.jpg';
+
+const LOGO_HEIGHT_PX = 75;
+
+const TitleBlock = styled.div`
+   display: block;
+   text-align: center;
+   padding-top: 15vh;
+   font-size: 1.5rem;
+   letter-spacing: 1.5rem;
+   text-shadow: 0.5rem 0.5rem 1rem rgba(0,0,0,0.35);
+   color: #666666;
+   text-transform: uppercase;
+   background-color: white;
+   align-items: center;
+   justify-content: center;
+   font-family: Arial;
+`;
+
+const LogoBlock = styled.div`
+   display: block;
+   height: ${LOGO_HEIGHT_PX}px;
+   width: fit-content;
+   margin: 2rem auto 25vh;
+`;
+
+const HSL_COOL_BLUE = 'hsla(200, 90%, 50%, 85%)';
+
+const ButtonBlock = styled.button`
+   display: block;
+   padding: 0.5rem 2rem;
+   margin: 0 auto;
+   background-color: ${HSL_COOL_BLUE};
+   color: white;
+   font-size: 1.25rem;
+   font-weight: bold;
+   border-radius: 0.25rem;
+   letter-spacing: 0.25rem;
+   text-transform: uppercase;
+   border: 0.15rem solid #888888;
+   box-shadow: 0.5rem 0.5rem 1rem rgba(0,0,0,0.5);
+   cursor: pointer;
+`;
+
+export function AppLoginPage(props) {
+   const login = useGoogleLogin({
+      onSuccess: codeResponse => console.log(codeResponse)
+   });
+   return [
+      <TitleBlock>{props.app_name}</TitleBlock>,
+      <LogoBlock>
+         <img src={logo} alt="am chill whale" height={LOGO_HEIGHT_PX}/>
+      </LogoBlock>,
+      <ButtonBlock onClick={() => login()}>Begin</ButtonBlock>
+   ]
+}
+
+export default AppLoginPage;
