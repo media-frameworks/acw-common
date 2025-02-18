581af4367941b13bba2ca59fae2554ece26fa1e5
media-frameworks <mikehallvideo@gmail.com>
2025-02-18 08:13:59 -0500

    CoolModal work

--- ui/CoolModal.jsx
+++ ui/CoolModal.jsx
@@ -1,44 +1,7 @@
 import React, {Component} from 'react';
-import styled from "styled-components";
 import PropTypes from 'prop-types';
 
-import {CoolStyles} from "common/ui/CoolImports";
-
-const FormField = styled.div`
-    position: fixed;
-    z-Index: 200;
-    padding-top: 5rem;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,35%);
-`;
-
-const FormContainer = styled.div`
-    margin: auto;
-    overflow: hidden;
-    border-radius: 0.5rem;
-    border: 0.15rem solid #aaaaaa;
-    box-shadow: 0.5rem 0.5rem 1rem rgba(0,0,0,25%);
-    min-width: 10%;   
-    background-color: white; 
-`;
-
-const FormContent = styled.div`
-    min-height: 5rem;
-    max-height: 90vh;
-`;
-
-export const ModalTitle = styled(CoolStyles.Block)`
-    ${CoolStyles.uppercase}
-    ${CoolStyles.centered}
-    ${CoolStyles.underline}
-    ${CoolStyles.bold}
-    letter-spacing: 0.125rem;
-    margin-bottom: 0.75rem;
-    font-size: 1.25rem;
- `;
+import {CoolModalStyles as styles} from './styles/CoolModalStyles';
 
 export class CoolModal extends Component {
 
@@ -47,11 +10,13 @@ export class CoolModal extends Component {
       response: PropTypes.func,
       width: PropTypes.string,
       settings: PropTypes.object,
+      title_text: PropTypes.string,
    }
 
    static defaultProps = {
       width: "40%",
-      settings: {}
+      settings: {},
+      title_text: null,
    }
 
    state = {
@@ -104,17 +69,31 @@ export class CoolModal extends Component {
       document.removeEventListener("click", this.click_handler);
    }
 
+   render_title = (title_text) => {
+      const {response} = this.props
+      const close_button = <styles.CloseButton
+         onClick={e => response(0)}>
+         {'X'}
+      </styles.CloseButton>
+      return <styles.ModalTitle>
+         {title_text}
+         {close_button}
+      </styles.ModalTitle>
+   }
+
    render() {
       const {modal_ref} = this.state;
-      const {width} = this.props;
+      const {width, title_text} = this.props;
       const style_extra = {width: width};
-      return <FormField>
-         <FormContainer style={style_extra} ref={modal_ref}>
-            <FormContent>
+      const title = title_text ? this.render_title(title_text) : [];
+      return <styles.FormField>
+         <styles.FormContainer style={style_extra} ref={modal_ref}>
+            {title}
+            <styles.FormContent>
                {this.props.contents}
-            </FormContent>
-         </FormContainer>
-      </FormField>
+            </styles.FormContent>
+         </styles.FormContainer>
+      </styles.FormField>
    }
 }
 
